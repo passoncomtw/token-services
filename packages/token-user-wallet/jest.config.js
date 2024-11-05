15c2c09e731216fdf359cf8187a6772e2b914946
TomasLin <horsekit1982@gmail.com>
2024-11-05 21:50:47 +0800

    chore: 增加 token wallet expo app

---
+++ packages/token-user-wallet/jest.config.js
@@ -0,0 +1,21 @@
+module.exports = {
+  displayName: 'token-user-wallet',
+  resolver: '@nx/jest/plugins/resolver',
+  preset: 'jest-expo',
+  moduleFileExtensions: ['ts', 'js', 'html', 'tsx', 'jsx'],
+  setupFilesAfterEnv: ['<rootDir>/src/test-setup.js'],
+  moduleNameMapper: {
+    '\\.svg$': '@nx/expo/plugins/jest/svg-mock',
+  },
+  transform: {
+    '.[jt]sx?$': [
+      'babel-jest',
+      {
+        configFile: __dirname + '/.babelrc.js',
+      },
+    ],
+    '^.+.(bmp|gif|jpg|jpeg|mp4|png|psd|svg|webp|ttf|otf|m4v|mov|mp4|mpeg|mpg|webm|aac|aiff|caf|m4a|mp3|wav|html|pdf|obj)$':
+      require.resolve('jest-expo/src/preset/assetFileTransformer.js'),
+  },
+  coverageDirectory: '../../coverage/packages/token-user-wallet',
+};
